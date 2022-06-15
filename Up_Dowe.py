c43d3957839942fad9a5be7fe5c38528421002fa
OmarAlKhalaf <99047548@mydavinci.nl>
2022-06-15 13:23:17 +0200

    (refresh)

--- Up_Dowe.py
+++ Up_Dowe.py
@@ -1,35 +1,57 @@
 # ========== Start ============ #
 from cProfile import label
+from cgitb import text
 import tkinter as tk
 from tkinter import CENTER, LEFT, TOP, Button, Label, LabelFrame, ttk
+
+
 root = tk.Tk()
 root.title('Up_down')
 root.geometry('400x400')
+root.config(bg='grey')
 # ======== fonction's ========= #
+num = 0
 
 
-# def num():
-#     pass
+def lab():
+    global window, num
+    window.config(text=f'{num}')
+    if num == 0:
+        root.config(bg='grey')
+    elif num > 0:
+        root.config(bg='green')
+    elif num < 0:
+        root.config(bg='red')
 
+
+def plus():
+    global num
+    print(num)
+    num = num + 1
+    lab()
+
+
+def minus():
+    global num
+    print(num)
+    num = num - 1
+    lab()
 # ======== Code ========= #
 
 
 # Up Button.
-up = Button(
-    root,
-    text="Up",
-    bg='green',
-    fg='white'
-).pack(fill='x', expand=True, ipady=30)
+up = Button(root, text="Up", bg='green', fg='white')
+up.pack(fill='x', expand=True, ipady=30)
+up.config(command=plus)
 
+# Window Label.
+window = Label(root, text=f'{num}')
+window.pack()
 
 # Down Button.
-down = Button(
-    root,
-    text='Down',
-    bg='red',
-    fg='white'
-).pack(fill='x', expand=True, ipady=30)
+down = Button(root, text='Down', bg='red', fg='white')
+down.pack(fill='x', expand=True, ipady=30)
+down.config(command=minus)
 
 
 # ========== End =========== #
